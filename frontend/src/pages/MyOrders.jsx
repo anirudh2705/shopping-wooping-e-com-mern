9d44cf3cb31634b1af5d06a2e7c4620ed0f16ef8
Anirudh <anirudhpatnaik2804@gmail.com>
2025-04-03 19:28:30 +0530

    added myOrders page

--- frontend/src/pages/MyOrders.jsx
+++ frontend/src/pages/MyOrders.jsx
@@ -1,7 +1,52 @@
+import { Button } from "@/components/ui/button";
 import React from "react";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { Badge } from "@/components/ui/badge";
 
 const MyOrders = () => {
-  return <div>myOrders</div>;
+  // const [first, setfirst] = useState([]);
+
+  return (
+    <div>
+      <Table>
+        <TableCaption>A list of your recent invoices.</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-[100px]">Image</TableHead>
+            <TableHead>Order Id</TableHead>
+            <TableHead>Created</TableHead>
+            <TableHead>Shipping Address</TableHead>
+            <TableHead>Items</TableHead>
+            <TableHead>Price</TableHead>
+            <TableHead>Status</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          <TableRow>
+            <TableCell className="font-medium">img</TableCell>
+            <TableCell>Paid</TableCell>
+            <TableCell>Credit Card</TableCell>
+            <TableCell>Credit Card</TableCell>
+            <TableCell>Credit Card</TableCell>
+            <TableCell>Credit Card</TableCell>
+            <TableCell>
+              <Badge variant="paid">Paid</Badge>
+            </TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+
+      <Button variant="red">shadcn</Button>
+    </div>
+  );
 };
 
 export default MyOrders;
